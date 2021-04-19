224017f73176fbb20b09a708efa0098b1c572481
Alinabuss <alina.buss@yahoo.de>
2021-04-19 15:03:42 +0200

    Loan-Anpassungen

--- Webapp/database/init.sql
+++ Webapp/database/init.sql
@@ -86,33 +86,18 @@ $$
 ;
 
 create or replace procedure new_loan(
-    loan_user int,
-    book_ids INT[],
-    duration int
+    book_id INT
 )
     language plpgsql
 AS
 $$
 DECLARE
-    book    INT;
     loan_id INT;
 BEGIN
 
-    INSERT INTO LOAN (ts_now, n_user_id)
-    VALUES (now(), loan_user)
+    INSERT INTO LOAN (ts_now, n_book_id)
+    VALUES (now(), book_id)
     RETURNING n_loan_id INTO loan_id;
 
-
-    FOREACH book IN ARRAY $2
-        LOOP
-            INSERT INTO BORROW_ITEM (n_duration, n_book_id, n_loan_id)
-            VALUES (duration, book, loan_id);
-
-            UPDATE books
-            SET b_is_available = false
-            WHERE n_book_id = book;
-        END loop;
-
-
 END;
-$$;
+$$; 
