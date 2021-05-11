2fd02ef6a5b89ef7f7e3fb113502a7723a5bb7d6
Phillip Lange <phillip@lange-privat.net>
2021-05-11 12:33:46 +0200

    added base cache for flask

--- Webapp/code/app/__init__.py
+++ Webapp/code/app/__init__.py
@@ -1,9 +1,11 @@
 # app/__init__.py
 
 from flask import Flask
+from flask_caching import Cache
 
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
+cache = Cache(app, config={'CACHE_TYPE': 'MemcachedCache', 'CACHE_MEMCACHED_SERVERS': ':6379/0'})
 
 # Load the config file
 app.config.from_object('config')
