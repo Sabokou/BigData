4a22f1e966a36e90bcbee2efe51dcb2f6659f597
Phillip Lange <phillip@lange-privat.net>
2021-09-02 22:43:10 +0200

    changed kafka consumer to make it add the streamed isbns to postgres

---
+++ Kafka/Consumer/src/book.py
@@ -0,0 +1,78 @@
+import logging
+import isbnlib
+
+
+class Book:
+
+    def __init__(self) -> object:
+        self.author_first_names = None
+        self.author_last_names = None
+        self.book_title = None
+        self.book_language = None
+        self.book_isbn = None
+        self.publishing_year = None
+
+    def set_via_isbn(self, s_isbn: str = "9780062893338"):
+        # remove "-" from isbn string
+        s_isbn = "".join(s_isbn.strip("-"))
+
+        # check if google books has information on the isbn
+        meta_google = isbnlib.meta(s_isbn, service='goob')
+        if bool(meta_google) is False:  # empty dict => False | not empty => use found meta_data
+            # fetch data from wiki api
+            logging.info("Book not found in Google API")
+            meta_wiki = isbnlib.meta(s_isbn, service='wiki')
+
+            # check if wikipedia api has information on the isbn
+            if bool(meta_wiki) is False:  # empty dict => False | not empty => use found meta_data
+                # fetch data from openlibrary api
+                logging.info("Book not found in Wiki API")
+                meta_open = isbnlib.meta(s_isbn, service='openl')
+
+                # check if openlibrary api has information on the isbn
+                if bool(meta_open) is False:  # empty dict => False | not empty => use found meta_data
+                    # if not => no information could be found on the isbn
+                    logging.warning("Book not found in any APIs")
+                    print("Book not found!")
+                    return None
+                else:
+                    meta_data = meta_open
+            else:
+                meta_data = meta_wiki
+        else:
+            meta_data = meta_google
+
+        self.author_first_names = []
+        self.author_last_names = []
+        for author in meta_data['Authors']:
+            name = author.split(" ")
+            last_name = name.pop(-1)
+            first_names = " ".join(name)
+            self.author_first_names.append(last_name)
+            self.author_last_names.append(first_names)
+
+        self.book_title = meta_data["Title"]
+        self.book_language = meta_data["Language"]
+        self.publishing_year = meta_data['Year']
+        self.book_isbn = list(meta_data.values())[0]
+
+    def get_s_sql_call(self) -> str or None:
+
+        if self.book_title is None:
+            return None
+        # if self.publisher_name is None:
+        #     return None
+        if self.author_last_names is None:
+            return None
+
+        call = f"""CALL add_book(
+                        ARRAY{self.author_first_names}, 
+                        ARRAY{self.author_last_names},
+                        {self.publishing_year}, 
+                        '{self.book_title}', 
+                        '{self.book_language}', 
+                        '{self.book_isbn}');"""
+
+        call = call.replace("'None'", "NULL").replace("None", "NULL")
+        # print(call)
+        return call
--- Kafka/Consumer/src/run.py
+++ Kafka/Consumer/src/run.py
@@ -1,9 +1,11 @@
 # this file is executed when the kafka consumer container is started
 # goal of the consumer is to read data from kafka streams
-
+import psycopg2
 from kafka import KafkaConsumer
 from time import sleep
 from json import loads
+from book import Book
+import logging
 
 # Init Kafka Producer with specific cluster config
 consumer = KafkaConsumer(
@@ -18,10 +20,51 @@ consumer = KafkaConsumer(
     enable_auto_commit=False
 )
 
+
+def add_book_to_database(isbn):
+    new_book = Book()
+    new_book.set_via_isbn(isbn)
+    add_new_book(new_book)
+
+
+def add_new_book(obj_book):
+    try:
+        call = obj_book.get_s_sql_call()
+        exec_statement(call)
+    except Exception as an_exception:
+        logging.error(an_exception)
+        logging.error("Book couldn't be added.")
+        return False
+    return True
+
+
+def exec_statement(sql: str):
+    """
+        can execute every kind of Sql-statement but does NOT return a response.
+
+        Use for:
+            - CALL Procedure
+            - UPDATE Statement
+        :param sql:
+        :return:
+        """
+    try:
+        psycopg2_connection = psycopg2.connect(database="postgres", user="postgres", port=5432,
+                                               password="1234", host="database")
+        db_cursor = psycopg2_connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        db_cursor.execute(sql)
+        psycopg2_connection.commit()
+        db_cursor.close()
+        return True
+    except psycopg2.errors.InFailedSqlTransaction:
+        logging.error("Transaction Failed - Review given inputs!")
+        return False
+
+
 for payload in consumer:
+    add_book_to_database(payload.value)
     print(
         f"################ \n \
         Received payload: {payload.value} \n \
         ################"
     )
-
